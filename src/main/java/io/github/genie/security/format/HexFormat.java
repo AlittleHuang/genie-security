9c86afb0fd33aa90f1cef56ebe069d09f1abea3f
huangchengwei <huang.chengwei@outlook.com>
2024-03-05 11:38:43 +0800

    增加hex字符串合法性验证

--- src/main/java/io/github/genie/security/format/HexFormat.java
+++ src/main/java/io/github/genie/security/format/HexFormat.java
@@ -3,6 +3,7 @@ package io.github.genie.security.format;
 import org.jetbrains.annotations.NotNull;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class HexFormat implements BinaryFormat {
     private final byte[] digits;
@@ -40,16 +41,24 @@ public class HexFormat implements BinaryFormat {
         byte[] dst = new byte[length];
         for (int i = 0; i < length; i++) {
             int pos = i << 1;
-            dst[i] = (byte) (VALUES[hex.charAt(pos)] << 4 | VALUES[hex.charAt(pos + 1)]);
+            dst[i] = (byte) (fromHexDigit(hex.charAt(pos)) << 4 | fromHexDigit(hex.charAt(pos + 1)));
         }
         return dst;
     }
 
+    public static int fromHexDigit(int ch) {
+        if ((ch >>> 7) == 0 && VALUES[ch] >= 0) {
+            return VALUES[ch];
+        }
+        throw new NumberFormatException("not a hexadecimal digit: \"" + (char) ch + "\" = " + ch);
+    }
+
     private static final byte[] DIGITS_LOWER = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
     private static final byte[] DIGITS_UPPER = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-    private static final byte[] VALUES = new byte['f' + 1];
+    private static final byte[] VALUES = new byte[128];
 
     static {
+        Arrays.fill(VALUES, (byte) -1);
         for (byte i = 0; i < DIGITS_LOWER.length; i++) {
             VALUES[DIGITS_LOWER[i]] = VALUES[DIGITS_UPPER[i]] = i;
         }
